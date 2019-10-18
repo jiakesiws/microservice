069d6196ab965293cde3711570ce1b89f96feb2a
torocket <spacecloud@foxmail.com>
2019-10-18 22:53:45 +0800

    新建服务提供者

---
+++ microservice-api/src/main/java/com/notfound/springcloud/entity/Dept.java
@@ -0,0 +1,25 @@
+package com.notfound.springcloud.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import java.io.Serializable;
+
+/**
+ * 序列化的原因是因为分布式需要将网络传输进行持久化操作。
+ * @author 404NotFoundx
+ * @date 2019/10/18 22:17
+ * @version 1.0.0
+ * @since 1.8
+ */
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@SuppressWarnings("serial")
+public class Dept implements Serializable {
+
+    private Long deptno; // 主键
+    private String deptName; // 部门名称
+    private String db_source; // 来自哪个数据库，因为微服务架构可以一个服务对应一个数据库
+
+}
