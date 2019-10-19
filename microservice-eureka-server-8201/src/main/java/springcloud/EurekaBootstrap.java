6d4cd5135d551095fcea42fdd32948fe775bc34e
torocket <spacecloud@foxmail.com>
2019-10-19 15:12:41 +0800

    eureka注册服务完善

---
+++ microservice-eureka-server-8201/src/main/java/springcloud/EurekaBootstrap.java
@@ -0,0 +1,21 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 404NotFoundx
+ * @date 2019/10/19 14:53
+ * @version 1.0.0
+ * @since 1.8
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaBootstrap.class, args);
+    }
+
+}
