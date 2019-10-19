dee05248ef65d3c139459699418fb220f8e5609e
torocket <spacecloud@foxmail.com>
2019-10-19 16:58:51 +0800

    服务发现

--- microservice-provider-dept-8001/src/main/java/springcloud/controller/DeptController.java
+++ microservice-provider-dept-8001/src/main/java/springcloud/controller/DeptController.java
@@ -1,9 +1,13 @@
 package springcloud.controller;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import springcloud.service.DeptService;
 import springcloud.vo.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author 404NotFoundx
@@ -18,19 +22,34 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
-    public Object add(@RequestBody Dept dept){
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Object add(@RequestBody Dept dept) {
         return deptService.add(dept);
     }
 
-    @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
-    public Object get(@PathVariable("id") Long id){
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public Object get(@PathVariable("id") Long id) {
         return deptService.get(id);
     }
 
-    @RequestMapping(value = "list",method = RequestMethod.GET)
-    public Object list(){
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public Object list() {
         return deptService.list();
+    }
+
+    @RequestMapping(value = "/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**************" + list);
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("MICROSERVICE-DEPT");
+        for (ServiceInstance element : serviceInstances) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort()
+                    + "\t" + element.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
